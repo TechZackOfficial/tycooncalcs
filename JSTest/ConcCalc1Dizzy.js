06e45362dc31bdfc9550f2d6062ab790555277f1
TechZack <44208067+TechZackOfficial@users.noreply.github.com>
2022-05-02 18:59:54 +0100

    Update ConcCalc1Dizzy.js

--- JSTest/ConcCalc1Dizzy.js
+++ JSTest/ConcCalc1Dizzy.js
@@ -8,11 +8,13 @@
         var PostOP = document.getElementById("PostOP");
 
 
-        if (premium.checked == true) {
-            trailerCapacity = trailerCapacity * 1.15;
+        if (premium.checked) {
+            trailerCapacity = Math.floor(trailerCapacity * 1.15);
+        } else if (PostOP.checked) {
+            trailerCapacity = Math.floor(trailerCapacity * 1.15);
         } else if (premium.checked || PostOP.checked) {
-            trailerCapacity = trailerCapacity * 1.3;
-       }
+            trailerCapacity = Math.floor(trailerCapacity * 1.3);
+        }
 
         // Debug for Checks
         console.log(trailerCapacity)
@@ -21,14 +23,18 @@
         const materialCalc = [ // calc for value : item name : weight : HTML Textbox ID
             [concreteWanted * 5, "Cement Mix", 50, "CMRequired"],
             [concreteWanted * 25, "Sand", 50, "CMSand"],
-            [concreteWanted * 10, "Sawdust", 50, "SawdustCalc"],
-            [concreteWanted, "Treated Water", 50, "TreatedWTextBox"],
-            [concreteWanted, "Untreated Water", 50, "UTWTextBox"],
-            [concreteWanted, "Acid", 50, "TWAcid"],
-            [Math.ceil(concreteWanted * 3.98), "Toxic Waste", 50, "TWTextBox"],
+            [concreteWanted * 10, "Sawdust - Cement Mix", 50, "CMSawdust"],
+            [concreteWanted * 10, "Sawdust - Logs", 50, "SawdustCalc"],
+            [concreteWanted, "Treated Water", 50, "TreatWRequired"],
+            [concreteWanted, "Treated Water", 50, "LCTWRequired"],
+            [concreteWanted, "Untreated Water", 50, "TreatedWUnfil"],
+            [concreteWanted, "Acid - Toxic Waste", 50, "TWAcid"],
+            [Math.ceil(concreteWanted * 3.98), "Toxic Waste", 50, "TWRequired"],
             [concreteWanted, "Logs", 50, "LogsRequired"],
-            [Math.ceil(concreteWanted * 3.5), "Quarry Rubble", 150, "QRRequired"],
-            [Math.ceil(concreteWanted * 41.6), "Gravel", 50, "QRGravel"]
+            [Math.ceil(concreteWanted * 5), "Quarry Rubble", 150, "QRRequired"],
+            [Math.ceil(concreteWanted * 20), "Ore Mix", 150, "QROM"],
+            [Math.ceil(concreteWanted * 60), "Gravel", 50, "QRGravel"],
+            [Math.ceil(concreteWanted * 5), "Emerald", 150, "QREmerald"]
         ]
 
 
@@ -41,8 +47,28 @@
                 console.log("ID not found!")
             }
 
+
+
+
+
+
             //debug block:
-            console.log("You'll need " + materialCalc[i][0] + " " + materialCalc[i][1] + " Which will take " +
-                runsRequired + " trips!</br>");
+            //console.log("You'll need " + materialCalc[i][0] + " " + materialCalc[i][1] + " Which will take " +
+            //    runsRequired + " trips!</br>");
         }
+
+       /*for (var i = 0; i < materialCalc.length; i++) {
+           var runsRequired = Math.ceil(materialCalc[i][2] * materialCalc[i][0] / trailerCapacity);
+           var calcPageIDs = document.getElementById(materialCalc[i][3]).id;
+           if(calcPageIDs) {
+               if (calcPageIDs.contains("Trips")) {
+                   document.getElementById(materialCalc[i][3]).value = runsRequired;
+               } else {
+                   document.getElementById(materialCalc[i][3]).value = materialCalc[i][0];
+               }
+           } else {
+               console.log("ID not found!")
+           }
+           console.log(calcPageIDs);
+       }*/
     }
