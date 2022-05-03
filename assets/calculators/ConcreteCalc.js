deaa55a9d7d0f9879e7647589712e52b8564c1ef
DizzyHurricane5001 <72887223+DizzyHurricane5001@users.noreply.github.com>
2022-05-03 11:30:15 +0100

    enhanced debug + cost per item needed

--- assets/calculators/ConcreteCalc.js
+++ assets/calculators/ConcreteCalc.js
@@ -51,48 +51,48 @@ function ConcreteCalc() {
 
     // BIDMAS
 
-    const materialCalc = [ // calc for value : item name : weight : HTML Textbox ID
+    const materialCalc = [ // calc for value : item name : weight : HTML Textbox ID : Cost Per item
         // Logs
-        [concreteWanted, "Logs", 60, "LogsRequired"],
-        [concreteWanted * 10, "Sawdust", 3, "SawdustCalc"],
+        [concreteWanted, "Logs", 60, "LogsRequired", 0],
+        [concreteWanted * 10, "Sawdust", 3, "SawdustCalc", 0],
         // Toxic Waste Calcs
-        [Math.ceil(concreteWanted / 4), "Toxic Waste", 110, "TWRequired"],
-        [Math.ceil(concreteWanted / 4) * 4, "Acid", 5, "TWAcid"],
-        [Math.ceil(concreteWanted / 4) * 2, "Scrap Lead", 15, "TWSLM"],
-        [Math.ceil(concreteWanted / 4) * 2, "Scrap Mercury", 15, "TWSM"],
+        [Math.ceil(concreteWanted / 4), "Toxic Waste", 110, "TWRequired", 0],
+        [Math.ceil(concreteWanted / 4) * 4, "Acid", 5, "TWAcid", 0],
+        [Math.ceil(concreteWanted / 4) * 2, "Scrap Lead", 15, "TWSLM", 0],
+        [Math.ceil(concreteWanted / 4) * 2, "Scrap Mercury", 15, "TWSM", 0],
         // Treated Water LS Route
-        [concreteWanted, "Unfiltered Water", 100, "TreatedWUnfil"],
-        [concreteWanted, "Treated Water", 100, "TreatWRequired"],
-        [concreteWanted, "Acid", 5, "TreatedWAcid"],
+        [concreteWanted, "Unfiltered Water", 100, "TreatedWUnfil", 0],
+        [concreteWanted, "Treated Water", 100, "TreatWRequired", 0],
+        [concreteWanted, "Acid", 5, "TreatedWAcid", 0],
         // Treated Water LC Route
-        [concreteWanted, "Treated Water", 100, "LCTWRequired"],
-        [concreteWanted, "Auto Parts", 100, "LCTWCar"],
-        [concreteWanted, "Military Goods", 100, "LCTWMG"],
-        [concreteWanted, "Export Goods", 100, "LCTWEG"],
-        [concreteWanted, "Liberty City Token", 0, "LCTWToken"],
+        [concreteWanted, "Treated Water", 100, "LCTWRequired", 0],
+        [concreteWanted, "Auto Parts", 100, "LCTWCar", 0],
+        [concreteWanted, "Military Goods", 100, "LCTWMG", 0],
+        [concreteWanted, "Export Goods", 100, "LCTWEG", 0],
+        [concreteWanted, "Liberty City Token", 0, "LCTWToken", 0],
         // Quarry Rubble Calcs
-        [Math.ceil(ceil10(((concreteWanted * 25) / 0.6), 1) / 12), "Quarry Rubble", 150, "QRRequired"],
-        [Math.ceil(ceil10(((concreteWanted * 25) / 0.6), 1) / 12) * 4, "Raw Ore Mix", 15, "QROM"],
-        [Math.ceil(ceil10(((concreteWanted * 25) / 0.6), 1) / 12) * 12, "Gravel", 5, "QRGravel"],
-        [Math.ceil(ceil10(((concreteWanted * 25) / 0.6), 1) / 12), "Raw Emeralds", 10, "QREmerald"],
+        [Math.ceil(ceil10(((concreteWanted * 25) / 0.6), 1) / 12), "Quarry Rubble", 150, "QRRequired", 0],
+        [Math.ceil(ceil10(((concreteWanted * 25) / 0.6), 1) / 12) * 4, "Raw Ore Mix", 15, "QROM", 0],
+        [Math.ceil(ceil10(((concreteWanted * 25) / 0.6), 1) / 12) * 12, "Gravel", 5, "QRGravel", 0],
+        [Math.ceil(ceil10(((concreteWanted * 25) / 0.6), 1) / 12), "Raw Emeralds", 10, "QREmerald", 0],
         // Gravel to Sand Filtration
-        [ceil10(((concreteWanted * 25) / 0.6), 1), "Gravel", 5, "GRRequired"],
-        [ceil10(((concreteWanted * 25) / 0.6), 1) / 10 * 6, "Sand", 5, "GRSand"],
-        [ceil10(((concreteWanted * 25) / 0.6), 1) / 10 * 4, "Flint", 5, "GRFlint"],
+        [ceil10(((concreteWanted * 25) / 0.6), 1), "Gravel", 5, "GRRequired", 0],
+        [ceil10(((concreteWanted * 25) / 0.6), 1) / 10 * 6, "Sand", 5, "GRSand", 0],
+        [ceil10(((concreteWanted * 25) / 0.6), 1) / 10 * 4, "Flint", 5, "GRFlint", 0],
         // Cement Mix
-        [concreteWanted * 5, "Cement Mix", 25, "CMRequired"],
-        [concreteWanted * 25, "Sand", 5, "CMSand"],
-        [concreteWanted * 10, "Sawdust", 3, "CMSawdust"],
+        [concreteWanted * 5, "Cement Mix", 25, "CMRequired", 0],
+        [concreteWanted * 25, "Sand", 5, "CMSand", 0],
+        [concreteWanted * 10, "Sawdust", 3, "CMSawdust", 0],
         // Concrete Calcs
 
-        [concreteWanted, "Concrete", 160, "ConRequired"],
-        [concreteWanted, "Treated Water", 100, "ConTW"], // Change ID to ConTW
-        [concreteWanted * 5, "Cement Mix", 25, "ConMix"],
+        [concreteWanted, "Concrete", 160, "ConRequired", 0],
+        [concreteWanted, "Treated Water", 100, "ConTW", 0], // Change ID to ConTW
+        [concreteWanted * 5, "Cement Mix", 25, "ConMix", 0],
         // Trips placeholders
         [Math.ceil(ceil10(((concreteWanted * 25) / 0.6), 1) / 12), "QRTrips", 150, "QRTrips"],
-        [concreteWanted, "TWTrips", 100, "TWTrips"],
-        [concreteWanted, "TreatedWTripsLosSantos", 100, "TreatedWTrips"],
-        [concreteWanted, "LogsTrips to Sawmill", 60, "LogsTrips"]
+        [concreteWanted, "TWTrips", 100, "TWTrips", 0],
+        [concreteWanted, "TreatedWTripsLosSantos", 100, "TreatedWTrips", 0],
+        [concreteWanted, "LogsTrips to Sawmill", 60, "LogsTrips", 0]
     ]
 
 
@@ -101,12 +101,11 @@ function ConcreteCalc() {
         if (calcPageIDs) {
             console.log(calcPageIDs);
             if (calcPageIDs.includes("Trips")) {
-                document.getElementById(materialCalc[i][3]).value = Math.ceil(materialCalc[i][2] * materialCalc[i][0] / trailerCapacity); // need weights for calc in array see placeholders
-            } else {
+                document.getElementById(materialCalc[i][3]).value = Math.ceil(materialCalc[i][2] * materialCalc[i][0] / trailerCapacity); 
                 document.getElementById(materialCalc[i][3]).value = materialCalc[i][0];
             }
         } else {
-            console.log("ID not found!")
+            console.log(CalcPageIDS + " ID not found!")
         }
     }
 }
